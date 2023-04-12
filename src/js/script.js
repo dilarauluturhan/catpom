00d92c4787d8b7f36090f0300b4e89309eeb251a
Dilara Uluturhan <dilarauluturhan@outlook.com>
2023-04-12 14:48:10 +0300

    JS codes completed

---
+++ src/js/script.js
@@ -0,0 +1,42 @@
+const startPom = document.querySelector('#start');
+const stopPom = document.querySelector('#stop');
+const resetPom = document.querySelector('#reset');
+const timerPom = document.querySelector('#timer');
+
+let interval;
+let timeLeft = 1500;
+
+function updateTimer() {
+    let minutes = Math.floor(timeLeft / 60);
+    let seconds = timeLeft % 60;
+    let formattedTime = `${minutes.toString().padStart(2, "0")}:${seconds.toString().padStart(2, "0")}`;
+
+    timerPom.innerHTML = formattedTime;
+}
+
+function startTimer() {
+    interval = setInterval(() => {
+        timeLeft--;
+        updateTimer();
+        if (timeLeft === 0) {
+            clearInterval(interval);
+            alert("Time's up!");
+            timeLeft = 1500;
+            updateTimer();
+        }
+    }, 1000)
+};
+
+function stopTimer() {
+    clearInterval(interval);
+};
+
+function resetTimer() {
+    clearInterval(interval);
+    timeLeft = 1500;
+    updateTimer();
+};
+
+startPom.addEventListener("click", startTimer);
+stopPom.addEventListener("click", stopTimer);
+resetPom.addEventListener("click", resetTimer);
